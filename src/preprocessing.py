5a3ace82cd5025eb78a73c4dc124cab0fa3c720e
TayyebaSadaq <tsadaq+tech@proton.me>
2026-09-18 16:53:54 +0100

    added functions instead of commenting out code and also did numerical summary of the data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,8 +7,13 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("data/diabetes.csv") # loading dataset
 
 ## Inspecting the dataset
-# print(df.head()) # prints out the first few rows of dataset
-# print(df.info()) # prints out summary of the dataset
-# print(df.isnull().sum()) # prints out the number of missing values in each column
+def inspect_dataset(df):
+    print(df.head()) # prints out the first few rows of dataset
+    print(df.info()) # prints out summary of the dataset
+    print(df.isnull().sum()) # prints out the number of missing values in each column
 
 ## Stat summary and visualisation of outliers
+def stat_summary(df):
+    print(df.describe()) # gives numeric summary of dataset
+
+    
